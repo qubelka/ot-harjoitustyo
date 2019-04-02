9814f052df15b4ea3908d3a3fbabc6f4b75604c2
Qubelka <42945433+qubelka@users.noreply.github.com>
2019-04-02 20:56:26 +0300

    tests modified

--- WeatherBot/src/test/java/BotTest.java
+++ WeatherBot/src/test/java/BotTest.java
@@ -1,6 +1,7 @@
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Before;
 import org.junit.runner.RunWith;
 import static org.mockito.ArgumentMatchers.isA;
 import org.mockito.Mock;
@@ -16,38 +17,46 @@ import tgbots.weatherbot.Bot;
 @RunWith(MockitoJUnitRunner.class)
 public class BotTest {
 
-    @Mock
-    Bot bot;
+//    @Mock
+//    Bot bot;
 
+    
+    Bot bot; 
+    
+    @Before
+    public void init() {
+        bot = new Bot();
+    }
+    
     @Test
     public void getUsername_ReturnsRigthUsername() {
-        Mockito.when(bot.getBotUsername()).thenReturn("HarjoitustyoWeatherbot");
+//        Mockito.when(bot.getBotUsername()).thenReturn("HarjoitustyoWeatherbot");
         assertEquals("HarjoitustyoWeatherbot", bot.getBotUsername());
-        verify(bot).getBotUsername();
+//        verify(bot).getBotUsername();
     }
 
     @Test
     public void getToken_ReturnsRightToken() {
-        Mockito.when(bot.getBotToken()).thenReturn("789067379:AAH7xM9S9Th-cKqdYbFowXIEBzb8vF3z3wo");
+//        Mockito.when(bot.getBotToken()).thenReturn("789067379:AAH7xM9S9Th-cKqdYbFowXIEBzb8vF3z3wo");
         assertEquals("789067379:AAH7xM9S9Th-cKqdYbFowXIEBzb8vF3z3wo", bot.getBotToken());
-        verify(bot).getBotToken();
+//        verify(bot).getBotToken();
     }
+//    
+//    @Test
+//    public void onUpdateReceived_IsCalledCorrectly() {
+//        doNothing().when(bot).onUpdateReceived(isA(Update.class));
+//        Update update = new Update();
+//        bot.onUpdateReceived(update);
+//        bot.onUpdateReceived(update);
+//        verify(bot, times(2)).onUpdateReceived(update);
+//    }
     
-    @Test
-    public void onUpdateReceived_IsCalledCorrectly() {
-        doNothing().when(bot).onUpdateReceived(isA(Update.class));
-        Update update = new Update();
-        bot.onUpdateReceived(update);
-        bot.onUpdateReceived(update);
-        verify(bot, times(2)).onUpdateReceived(update);
-    }
-    
-    @Test
-    public void getMainMenuKeyboard_ReturnsCorrectKeyboard() {
-        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
-        Mockito.when(bot.getMainMenuKeyboard()).thenReturn(replyKeyboardMarkup);
-        assertEquals(replyKeyboardMarkup, bot.getMainMenuKeyboard());
-        verify(bot).getMainMenuKeyboard();
-    }
+//    @Test
+//    public void getMainMenuKeyboard_ReturnsCorrectKeyboard() {
+//        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+//        Mockito.when(bot.getMainMenuKeyboard()).thenReturn(replyKeyboardMarkup);
+//        assertEquals(replyKeyboardMarkup, bot.getMainMenuKeyboard());
+//        verify(bot).getMainMenuKeyboard();
+//    }
     
 }
